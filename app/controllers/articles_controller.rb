848b128408514e5b78207d26d9e69a1ca704dc1d
Kuczynior <maksymkuczynski@gmail.com>
2023-01-07 15:14:39 +0100

    Addect whole moderation option to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,48 @@
 class ArticlesController < ApplicationController
 
     def index
-        @articles= Article.all
+      @articles= Article.all
     end
+  
     def new
-        @article= Article.new
-    end
-    def create
-        @article= Article.new(article_params)
-        if @article.save
-            redirect_to articles_path
-        else 
-            render :new
-        end
-    end
-    def article_params
-        params.require(:article).permit(:title, :body)
+      @article= Article.new
     end
 
+    def create
+      @article= Article.new(article_params)
+
+      if @article.save
+        redirect_to articles_path
+      else 
+        render :new
+      end
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        redirect_to articles_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      @article=Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+    def destroy
+      @article=Article.find(params[:id])
+      redirect_to articles_path
+    end
 
 end
