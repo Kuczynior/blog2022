f19d36f0b704f6283f45aa9735802e3e659c1d22
Kuczynior <maksymkuczynski@gmail.com>
2023-02-15 15:08:29 +0100

    user edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,7 +7,17 @@ module Admin
     def new
       @user = User.new
     end
-        
+
+    def create
+      @user= User.new(user_params)
+      
+      if @user.save
+        redirect_to admin_user_path(@user)
+      else 
+        render :new
+      end
+    end
+      
     def edit
       @user = User.find(params[:id])
     end
